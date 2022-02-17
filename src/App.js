1209b6050b218a49b1cb4bf162e4cc17cb8668d8
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-02-17 15:52:01 +0100

    handling Data change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import { Cards, Chart, CountryPicker } from './components';
+import { Cards, Chart, CountryPicker, DatePicker } from './components';
 import styles from './App.module.css';
 
-import { fetchData } from './api';
+import { fetchData, fetchDailyData } from './api';
 
 import coronaImage from './images/image.png';
 
@@ -11,6 +11,7 @@ class App extends React.Component {
     state = {
         data: {},
         country: '',
+        date: ''
     }
 
     async componentDidMount() {
@@ -23,17 +24,28 @@ class App extends React.Component {
         const fetchedData = await fetchData(country);
         
         this.setState({ data: fetchedData, country: country })
+
+        // console.log("Country data fetched : ", fetchedData);
+    }
+
+    handleDateChange = async (date) => {
+        const fetchedData = await fetchData(date);
+        
+        this.setState({ data: fetchedData, date: date })
+
+        // console.log("Date daata fetched : ", fetchedData);
     }
 
     render() {
-        const { data, country } = this.state;
+        const { data, country, date } = this.state;
 
         return (
             <div className={styles.container}>
                 <img className={styles.image} src={coronaImage} alt="COVID-19"/>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart data={data} country={country}/>
+                <DatePicker handleDateChange={this.handleDateChange} />
+                <Chart data={data} country={country} date={date}/>
             </div>
         )
     }
