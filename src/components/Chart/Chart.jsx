55e0def15470ca4ed5a8302e0856331e26dfefb5
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-02-17 21:05:56 +0100

    adding loading animation

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
 import { Chart as ChartJS } from 'chart.js/auto'
 import { Line, Bar } from 'react-chartjs-2';
+import { Grid, Box, CircularProgress } from '@material-ui/core';
 
+import cx from 'classnames';
 import styles from './Chart.module.css';
 
 const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
@@ -26,22 +28,31 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
                         data: dailyData.map(({ confirmed }) => confirmed),
                         label: 'Infected',
                         borderColor: 'blue',
+                        backgroundColor: 'rgba(0, 0, 250, 0.1)',
                         fill: true,
                     }, {
                         data: dailyData.map(({ deaths }) => deaths),
                         label: 'Deaths',
                         borderColor: 'red',
-                        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                        backgroundColor: 'rgba(250, 0, 0, 0.2)',
                         fill: true,
                     }, {
                         data: dailyData.map(({ incidentRate }) => incidentRate),
                         label: 'Incident rate',
                         borderColor: 'orange',
-                        backgroundColor: 'rgba(255, 100, 100, 0.5)',
+                        backgroundColor: 'rgba(250, 100, 0, 0.2)',
                         fill: true,
                     }],
                 }}
-            />) : null
+            />) : (
+            <div className={styles.container}>
+                <Grid container justifyContent="center">
+                    <Box className={cx(styles.loader)}>
+                        <CircularProgress />
+                    </Box>
+                </Grid>
+            </div>
+        )
     );
 
     const barChart = (
@@ -65,7 +76,15 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
                         title: { display: true, text: `Current state in ${country}`},
                     }}
                 />
-            ) : null
+            ) : (
+            <div className={styles.container}>
+                <Grid container justifyContent="center">
+                    <Box className={cx(styles.loader)}>
+                        <CircularProgress />
+                    </Box>
+                </Grid>
+            </div>
+        )
     )
 
     return (
